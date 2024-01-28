64b34fabe2452e079a6c6b69ff9aa16426e4fdd1
Eduardo Souza <denis.souzadeoliveira05@gmail.com>
2024-01-28 12:15:24 -0300

    Refinando o calculo de números primos

--- Beginner/The_Coins_Robbie.js
+++ Beginner/The_Coins_Robbie.js
@@ -2,6 +2,9 @@ const readline = require('readline');
 let arr = [];
 let min = 2;
 let max = 20;
+let Nvalue;
+let sumValue;
+let arrN = [];
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -15,7 +18,6 @@ rl.on('line', (N) =>{
     let Vi;
     let number_Gloria;
     let M_arr = [];
-    console.log(M)
 
     for(i = 1; i < M; i++){
         Vi = Math.floor((Math.random() * (500 - 1 + 1)) + 1);
@@ -23,8 +25,9 @@ rl.on('line', (N) =>{
     }
     if(arr.length > 0){
       number_Gloria = arr[0];
+      Nvalue = arr[0];
       if(number_Gloria > 1 && number_Gloria < M){
-        The_Coins_Of_Robbie(number_Gloria,M_arr)
+        SearchForSalt(M_arr)
       }    
 
      arr = [];
@@ -33,46 +36,31 @@ rl.on('line', (N) =>{
 
 })
 
-function The_Coins_Of_Robbie(N,arr_coin){
-console.log("Arr before the slice " + arr_coin)
-let arrJumpN = arr_coin.slice(N)
-// console.log("Value off N " + N)
-// console.log(arrJumpN)
-let arrN = [];
-
-//Vou chamar uma função recursiva para retornar os valores do saldo
-
 
 function SearchForSalt(arr){
-  let arrPure = arr
-for(let keys of arr){
-  if(arr[keys] != undefined){
-    let arrSaltValue = arr.slice(N);
-    arrN.push(arrSaltValue)
-    // arr.shift();
-    SearchForSalt(arrSaltValue)
+  if(arr[Nvalue] != arr[undefined]){
+    arrN.push(arr[Nvalue])
+    arr = arr.slice(Nvalue)
+    sumValue = arrN.reduce((acumulator, currentValue) => acumulator + currentValue,0,) 
+    return SearchForSalt(arr)
   }else{
-    return null
+    PrimeNumbers(sumValue) 
+    return arr[undefined];
   }
-  // console.log("Valor de values " + values)
+
+ 
 }
 
-console.log(arrN)
-
+function PrimeNumbers(value){
+  arrN = [];
+  if(value % 2 == 0 || value % 3 == 0){
+    //Se for divisivel por dois ou por três significa que não é primo
+    console.log("Bad boy! I’ll hit you.")
+  }else{
+    console.log("You’re a coastal aircraft, Robbie, a large silver aircraft.")
+    //Faz o calculo do valor e retorna as conversas exigidas no exercício
+  }
 }
 
-SearchForSalt(arr_coin)
 
-console.log("Valor do array de arrN " + arrN)
-
-// //Pegando númereos primos
-// if(value % 2 == 0 || value % 3 == 0){
-//   //Se for divisivel por dois ou por três significa que não é primo
-// }else{
-//   //Faz o calculo do valor e retorna as conversas exigidas no exercício
-// }
-
-
-}
-
-//AINDA NÃO FOI FEITO O TESTE
+//TESTE NÃO PASSOU (WRONG ANSWER (45%)) -- VOU AJUSTAR O CALCULO DE NÚMEROS PRIMOS --
--- README.md
+++ README.md
@@ -7,3 +7,12 @@ Este repositório contém minhas soluções para os exercícios resolvidos no Be
 O repositório está organizado de acordo com as categorias de exercícios no Beecrowd. Cada diretório representa uma categoria específica e contém os códigos fonte das soluções correspondentes.Conforme for atualizando separo por níveis também
 Por agora tem poucos mas a medida que eu for fazendo vou atualizando aqui
 
+## Regras para contribuir
+
+- Deve conter se foi resolvido ou não;
+
+- Se foi resolvido adicionar o Runtime do código;
+
+- Se foi rejeitado colocar o motivo (Compiation error, Runtime error, Time limit exceeded, Presentation Error, Wrong Answer, Possible runtime error, Memory limit exceeded);
+
+- Separar os exercícios conforme as pastas;
